63240f55d28270f07811cab5fa0ecb2bf294288a
npvinh04 <nqvinh1995@gmail.com>
2022-06-16 23:31:43 +0700

    update profile card part 1

---
+++ client/src/components/profileCard/profileCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Cover from "../../img/cover.jpg";
+import Profile from "../../img/profileImg.jpg"
+
+const ProfileCard = () => {
+    return (
+        <div className="ProfileCard">
+            <div className="ProfileImages">
+                <img src={Cover} alt=""/>
+                <img src={Profile} alt=""/>
+            </div>
+        </div>
+    )
+}
+
+export default ProfileCard;
--- client/src/components/profileSide/ProfileSide.jsx
+++ client/src/components/profileSide/ProfileSide.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import LogoSearch from "../logoSearch/LogoSearch";
+import ProfileCard from "../profileCard/profileCard";
 
 const ProfileSide = () => {
     return (
         <div className="ProfileSide">
             <LogoSearch/>
-            Profile
+            <ProfileCard/>
         </div>
     )
 };
