1270fb0985564db5a7eb17f5333e4850277a33d0
Vinh Nguyen (BnK - HN) <vinh.nguyen@bnksolution.com>
2022-06-16 08:48:47 +0700

    update client home

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <div className="profileSide"></div>
+            <div className="postSide"></div>
+            <div className="rightSide"></div>
+        </div>
+    )
+};
+
+export default Home;
