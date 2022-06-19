156c0d6231ba33cc431ef5b49f12ff577be2895c
npvinh04 <nqvinh1995@gmail.com>
2022-06-19 23:54:06 +0700

    Right Side

---
+++ client/src/components/RightSide/RightSide.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './RightSide.css';
+import Home from '../../img/home.png';
+import Noti from '../../img/noti.png';
+import Comment from '../../img/comment.png';
+import { UilSetting} from "@iconscout/react-unicons";
+
+const RightSide = () => {
+    return (
+        <div className="RightSide">
+
+        </div>
+    )
+}
+
+export default RightSide;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import './Home.css'
 import ProfileSide from "../../components/ProfileSide/ProfileSide";
 import PostSide from "../../components/PostSide/PostSide";
+import RightSide from "../../components/RightSide/RightSide";
 
 const Home = () => {
     return (
         <div className="Home">
             <ProfileSide/>
             <PostSide/>
-            <div className="rightSide">Right Side</div>
+            <RightSide/>
         </div>
     )
 };
