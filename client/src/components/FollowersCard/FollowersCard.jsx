baa1888ddcc82511f966763e4a231c9a6a916274
npvinh04 <nqvinh1995@gmail.com>
2022-06-19 10:02:06 +0700

    update Followers Card

--- client/src/components/FollowersCard/FollowersCard.jsx
+++ client/src/components/FollowersCard/FollowersCard.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { Followers } from "../../Data/FollowersData";
 import './FollowersCard.css';
 
 const FollowersCard = () => {
     return(
         <div className="FollowerCard">
             <h3>Who is following you</h3>
+
+            {Followers.map((follower, id)=>{
+                return(
+                    <div className="follower">
+                        <div>
+                            <img src={follower.img} alt="" className='followerImage' />
+                            <div className="name">
+                                <span>{follower.name}</span>
+                                <span>@{follower.username}</span>
+                            </div>
+                        </div>
+                        <button className="button fc-button">
+                            Follow
+                        </button>
+                    </div>
+                )
+            })}
         </div>
     )
 }
