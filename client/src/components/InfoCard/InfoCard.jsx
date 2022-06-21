a035fe0812eff91e48a11e597133bbba6bade6d5
Vinh Nguyen (BnK - HN) <vinh.nguyen@bnksolution.com>
2022-06-21 08:21:22 +0700

    update Info Card and Profile Left

--- client/src/components/InfoCard/InfoCard.jsx
+++ client/src/components/InfoCard/InfoCard.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 import './InfoCard.css';
+import {UilPen} from "@iconscout/react-unicons";
 
 
 const InfoCard = () => {
     return (
-        <div>
-            Info Card
+        <div className="InfoCard">
+            <div className="infoHead">
+                <h4>Your Info</h4>
+                <UilPen/>
+            </div>
+
+            <div className="info">
+                <span>
+                    <b>Status in</b>
+                </span>
+                <span>Relationship</span>
+            </div>
+
+            <div className="info">
+                <span>
+                    <b>Lives in</b>
+                </span>
+                <span>Multan</span>
+            </div>
+
+            <div className="info">
+                <span>
+                    <b>Works at</b>
+                </span>
+                <span>Duy Tan - Viet Nam</span>
+            </div>
+
+            <button className="button lg-button">Logout</button>
         </div>
     )
 }
