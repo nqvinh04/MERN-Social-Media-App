85776989d64cc2b0be5990cf546916229a78ffd3
npvinh04 <nqvinh1995@gmail.com>
2022-06-28 23:29:24 +0700

    update auth jwt

--- api/src/Controllers/AuthController.js
+++ api/src/Controllers/AuthController.js
@@ -1,5 +1,6 @@
 import UserModel from "../Models/userModel.js";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 
 // Registering a new User
@@ -15,10 +16,15 @@ export const registerUser = async (req, res) => {
         if(oldUser){
             return res.status(400).json({message: "username is already registered!"})
         }
-        await newUser.save();
-        res.status(200).json(newUser)
+        const user = await newUser.save();
+
+        const token = jwt.sign({
+            username: user.username,
+            id: user._id
+        }, process.env.JWT_KEY, {expiresIn: '1h'})
+        res.status(200).json({user, token})
     } catch (error) {
-        res.status(500).json({message: error.message});
+        res.status(500).json({ message: error.message });
     }
 };
 
@@ -27,12 +33,20 @@ export const loginUser = async (req, res) => {
     const {username, password} = req.body
 
     try {
-        const user = await UserModel.findOne({username: username})
+        const user = await UserModel.findOne({ username: username });
 
         if(user){
-            const validity = await bcrypt.compare(password, user.password)
+            const validity = await bcrypt.compare(password, user.password);
 
-            validity ? res.status(200).json(user) : res.status(400).json("Wrong Password");
+            if (validity && password !== user.password){
+                res.status(400).json("Wrong password")
+            } else {
+                const token = jwt.sign({
+                    username: user.username,
+                    id: user._id
+                }, process.env.JWT_KEY, {expiresIn: '1h'})
+                res.status(200).json({user, token})
+            }
         } else {
             res.status(404).json("User does not exists");
         }
