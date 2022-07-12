b952a24ffa8fb1ee6a373f4ea44eb1f6a3a3b757
Vinh Nguyen (BnK - HN) <vinh.nguyen@bnksolution.com>
2022-07-12 09:40:23 +0700

    chat app part 1.1

--- api/src/Models/messageModel.js
+++ api/src/Models/messageModel.js
@@ -2,7 +2,17 @@ import mongoose from "mongoose";
 
 
 const MessageSchema = new mongoose.Schema({
-
+    chatId: {
+        type: String
+    },
+    senderId: {
+        type: String
+    },
+    text: {
+        type: String
+    }
+},{
+    timestamps: true,
 });
 
 const MessageModal = mongoose.model("", MessageSchema);
