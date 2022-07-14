e389c859de34219c7e44ce610a16bb58a94046b9
Vinh Nguyen (BnK - HN) <vinh.nguyen@bnksolution.com>
2022-07-14 08:30:33 +0700

    font end

--- client/src/components/ChatBox/ChatBox.jsx
+++ client/src/components/ChatBox/ChatBox.jsx
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from "react";
 import {getUser} from "../../api/UseRequest";
+import "./ChatBox.css";
 
 
 const ChatBox = ({chat, currentUserId}) => {
 
     const [userData, setUserData] = useState(null);
+    const serverPublic = 'http://localhost:5001/images/';
 
     // fetching data for header
     useEffect(() => {
@@ -18,16 +20,36 @@ const ChatBox = ({chat, currentUserId}) => {
                 console.log('Chat Box error', error)
             }
         };
-        if(chat!==null){
+        if (chat !== null) {
             getUserData();
         }
     }, [chat, currentUserId]);
-    
+
 
     return (
         <>
             <div className="ChatBox-container">
-                Chat Box
+                <>
+                    <div className="chat-header">
+                        <div className="follower">
+                            <div>
+                                {/*<div className="online-dot"/>*/}
+                                <img src={userData?.profilePicture
+                                    ? serverPublic + userData.profilePicture
+                                    : serverPublic + "defaultProfile.png"}
+                                     alt=""
+                                     className='followerImage'
+                                     style={{width: '50px', height: '50px'}}
+                                />
+                                <div className="name" style={{fontSize: "0.8rem"}}>
+                                    {/*<span>{userData?.firstname} {userData?.lastname}</span>*/}
+                                    {/*<span>Online</span>*/}
+                                </div>
+                            </div>
+                        </div>
+                        <hr style={{width: '85%', border: '0.1px solid #ececec'}}/>
+                    </div>
+                </>
             </div>
         </>
     )
